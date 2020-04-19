97171872fa22554d3ee10f4c792556886ee10cc7
Freddie Ager <frederick@spirables-air.mynet>
2020-04-19 23:00:45 +0100

    Create admin user after database migration

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 pip install -r requirements.txt
 
-# Prompts the user to create an admin account
-python manage.py createsuperuser
-
 # Perform initial migration and populate database with sample data
 python manage.py migrate
 python manage.py populate_data
+
+# Prompts the user to create an admin account
+python manage.py createsuperuser
 
 # Starts the server on 127.0.0.1:8000
 python manage.py runserver
